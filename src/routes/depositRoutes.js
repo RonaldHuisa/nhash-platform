f08eb94f0195d1b11f83f8791507440cdb0812e9
Ronald Huisa <119126048+RonaldHuisa@users.noreply.github.com>
2026-05-13 11:45:16 -0500

    Delete src directory

--- src/routes/depositRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
-const { scanMyDeposits } = require("../controllers/depositController");
-
-const router = express.Router();
-
-router.post("/scan-me", authMiddleware, scanMyDeposits);
-
-module.exports = router;
--- src/routes/marketRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getPrices } = require("../controllers/marketController");
-
-const router = express.Router();
-
-router.get("/prices", getPrices);
-
-module.exports = router;
